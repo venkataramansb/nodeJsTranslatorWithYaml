d9ee1f5303dfd048c1f0ca3845909a41bcb29f39
PRODAPT\venkataraman.sb <venkataraman.sb@prodapt.com>
2021-01-08 18:07:13 +0530

    Updated multiple ways of Translation

--- translation.js
+++ translation.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const yaml = require('js-yaml');
 
-var lang = 'french';
+var lang = 'tamil';
 var inputRequestJson = 'sampleJson.json';
-var outputJson = 'translatedResponse.json';
+var outputJson = 'translatedOutputResponse.json';
 
 translator(lang, inputRequestJson);
 
@@ -11,23 +11,37 @@ function translator(lang, inputRequestJson) {
 
     fs.readFile(inputRequestJson, function (err, data) {
         if (err) throw err;
+        try {
+            let users = JSON.parse(data);
+            let fileContents = fs.readFileSync(`translate-${lang}.yaml`, 'utf8');
+            let ymldata = yaml.load(fileContents);
 
-        let users = JSON.parse(data);
-        let newdata = JSON.stringify(users)
-        let fileContents = fs.readFileSync(`translate-${lang}.yaml`, 'utf8');
-        let ymldata = yaml.load(fileContents);
+            var translatedData = replace(users, ymldata)
 
-        Object.keys(newdata).forEach(function (value) {
-            Object.keys(ymldata).find(function (key) {
-                if (newdata.includes(key)) {
-                    newdata = newdata.replace(key, ymldata[key])
-                }
-            });
-        });
-        // console.log(newdata)
-        fs.writeFileSync(outputJson, newdata);
+            fs.writeFileSync(outputJson, JSON.stringify(translatedData));
+        } catch (e) {
+            console.log(e);
+        }
     });
-
 }
+
+function replace(obj,ymldata){
+    for(let key in obj)
+      if(typeof obj[key] == 'object'){
+        replace(obj[key],ymldata);
+      }
+    else {
+        Object.keys(ymldata).find(function(value){
+            if(typeof obj[key] == 'string'){
+             if(obj[key].includes(value)){
+                 obj[key] = obj[key].replace(value,ymldata[value]);
+               
+             }
+            }
+        });
+    }
+    return obj;
+  }
+
 
 
---
+++ translation_value.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const yaml = require('js-yaml');
+
+var lang = 'tamil';
+var inputRequestJson = 'sampleJson.json';
+var outputJson = 'translatedResponse.json';
+
+translator(lang, inputRequestJson);
+
+function translator(lang, inputRequestJson) {
+
+    fs.readFile(inputRequestJson, function (err, data) {
+        if (err) throw err;
+
+        let users = JSON.parse(data);
+        let newdata = JSON.stringify(users)
+        let fileContents = fs.readFileSync(`translate-${lang}.yaml`, 'utf8');
+        let ymldata = yaml.load(fileContents);
+
+        Object.keys(newdata).forEach(function (value) {
+            Object.keys(ymldata).find(function (key) {
+                if (newdata.includes(key)) {
+                    newdata = newdata.replace(key, ymldata[key])
+                }
+            });
+        });
+     
+        fs.writeFileSync(outputJson, newdata);
+    });
+
+}
